2ac592cbd873902b3d67ad89635d5230568a9d37
Isaac Lum <zisaac2468@gmail.com>
2024-06-25 02:23:33 +0800

    Edited register to include accountNum and created transfer route

--- application/routes.py
+++ application/routes.py
@@ -5,10 +5,14 @@ from flask import render_template, request, flash, redirect, json, jsonify, url_
 from flask_login import login_user, login_required, current_user, logout_user, AnonymousUserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from application.api import *
+from datetime import datetime
 import math
 
 with app.app_context():
     db.create_all()
+
+# global accountIdIncrement
+accountIdIncrement = 9000000000
 
 #Handles http://127.0.0.1:5000/
 @app.route('/') 
@@ -49,6 +53,8 @@ def register():
     # Retrieve the register form
     form = registerForm()
     if request.method == 'POST':
+        # Generate accountId
+        
         # Retrieve the full name from the form sent
         fullname = form.fullname.data
         # Retrieve the email from the form sent
@@ -71,12 +77,15 @@ def register():
                 # If the username already exists in the database we refresh the page and display an error message
                 flash('Username already exists', 'warning')
                 return redirect(url_for('register'))
-                return redirect(url_for('register'))
             
             # Create a new user in the database
-            new_user = User(email = email, username = username, password = generate_password_hash(password, method='pbkdf2:sha256'), fullname = fullname, balance = 0.00)
-
+            new_user = User(email = email, accountId = 0, username = username, password = generate_password_hash(password, method='pbkdf2:sha256'), fullname = fullname, balance = 10.00)
             db.session.add(new_user)
+            db.session.commit()
+            
+            # Retrieve the user_id that was just created and use it to generate and update the user's account id
+            user = User.query.filter_by(user_id = new_user.user_id).first()
+            user.accountId = User.user_id + accountIdIncrement
             db.session.commit()
 
             # Direct the user to the login page and display a success message
@@ -103,6 +112,61 @@ def transaction():
     flash("Please login", "warning")
     return redirect(url_for("index"))
 
+# Transfer page
+# You need to be logged in to use it
+@app.route('/transfer', methods=['GET', 'POST'])
+def transfer():
+    form = transferForm()
+    if current_user.is_authenticated:
+        form = transferForm()
+        if request.method == 'POST':
+            if form.validate_on_submit():
+                accountNum = int(form.accountNum.data)
+                amount = float(form.amount.data)
+                
+                currUserId = current_user.user_id
+
+                user = User.query.filter_by(user_id = currUserId).first()
+                user2 = User.query.filter_by(accountId = accountNum).first()
+                userBal = float(user.balance)
+
+                if user.accountId == accountNum:
+                    flash(f"Cannot transfer money to yourself","warning")
+                    return render_template("transfer.html", form = form, title = 'Transfer')
+
+                if amount <= 0:
+                    flash(f"Invalid balance","warning")
+                    return render_template("transfer.html", form = form, title = 'Transfer')
+
+                if userBal < amount:
+                    flash(f"Insufficient balance","warning")
+                    return render_template("transfer.html", form = form, title = 'Transfer')
+
+                recipient = User.query.filter_by(accountId = accountNum).first()
+
+                if not recipient:
+                    flash(f"User does not exist","warning")
+                    return render_template("transfer.html", form = form, title = 'Transfer')
+                
+                user.balance = str(userBal - amount)
+                recipient.balance = str(float(recipient.balance) + amount)
+                
+                timeNow = datetime.now()
+                userTransaction = Transaction(amount = str(-1 * amount), type = "Transfer", accountId = accountNum, date = timeNow,fk_user_id = currUserId)
+                recipientTransaction = Transaction(amount = str(amount), type = "Transfer", accountId = user.accountId, date = timeNow, fk_user_id = user2.user_id)
+
+                db.session.add(userTransaction)
+                db.session.add(recipientTransaction)
+                db.session.commit()
+                flash(f"${amount} was successfully transfered to {accountNum}","success")
+                return render_template("transfer.html", form = form, title = 'Transfer')
+            else:
+                flash("Invalid Account Number or Balance. Please check again!","warning")
+                return render_template("transfer.html", form = form, title = 'Transfer')
+        return render_template("transfer.html", form = form, title = 'Transfer')
+    flash("Please login", "warning")
+    return redirect(url_for("login"))
+
 # Logout button
 # You need to be logged in to use it
 @app.route('/logout')
@@ -112,4 +176,4 @@ def logout():
         logout_user()
         return redirect(url_for("index"))
     flash("Please login", "warning")
-    return redirect(url_for("index"))
+    return redirect(url_for("login"))
